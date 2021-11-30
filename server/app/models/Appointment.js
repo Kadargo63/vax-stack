801c0ead81defce6a2abe132f7fb1e01782323b5
Robert Isaacs <robertisaacs87@yahoo.com>
2021-11-29 22:24:41 -0600

    added appointment model

---
+++ server/app/models/Appointment.js
@@ -0,0 +1,22 @@
+module.exports = (sequelize, Sequelize) => {
+    const Appointment = sequelize.define("appointments", {
+        location: {
+            type: Sequelize.STRING,
+            allowNull: false,
+        },
+        date: {
+            type: Sequelize.DATEONLY,
+            allowNull: false,
+        },
+        time: {
+            type: Sequelize.TIME,
+            allowNull: true,
+        },
+        details: {
+            type: Sequelize.TEXT,
+            allowNull: true,
+        },
+    });
+
+    return Appointment;
+};
